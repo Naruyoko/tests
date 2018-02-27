c6c9c148af67b16cdad7465dbba95e0da46c70f5
Naruyoko <32781397+Naruyoko@users.noreply.github.com>
2018-02-27 08:23:37 -0500

    Update expMath.js

--- expMath/expMath.js
+++ expMath/expMath.js
@@ -8,5 +8,6 @@ expMath=(function () {
     div:function (a,b){return a-b;},
     pow:function (a,b){return a*10^b;},
     root:function (a,b){return a/10^b;},
+    text:function (a,m){return string(10^(a-Math.floor(a)))+m+string(Math.floor(a))
   };
 }());
