9ad01229db8d0fe1c047f5afc000ed9d4ae773d9
Naruyoko <32781397+Naruyoko@users.noreply.github.com>
2018-02-27 08:11:41 -0500

    Update expMath.js

--- expMath/expMath.js
+++ expMath/expMath.js
@@ -1,6 +1,18 @@
-export function exp_add(a,b){return Math.max(a,b)+log10(1+10^(-Math.abs(a-b)));}
-export function exp_sub(a,b){return Math.max(a,b)+log10(1-10^(-Math.abs(a-b)));}
-export function exp_mult(a,b){return a+b;}
-export function exp_div(a,b){return a-b;}
-export function exp_pow(a,b){return a*10^b;}
-export function exp_root(a,b){return a/10^b;}
+'use strict';
+var modules=expMath||{};
+expMath=(function () {
+  var add=function exp_add(a,b){return Math.max(a,b)+log10(1+10^(-Math.abs(a-b)));};
+  var sub=function (a,b){return Math.max(a,b)+log10(1-10^(-Math.abs(a-b)));};
+  var mult=function (a,b){return a+b;};
+  var div=function (a,b){return a-b;};
+  var pow=function (a,b){return a*10^b;};
+  var root=function (a,b){return a/10^b;};
+  return{
+    add:add
+    sub:sub
+    mult:mult
+    div:div
+    pow:pow
+    root:root
+  };
+}());
