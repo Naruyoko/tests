10a7c51483b328568e31496bb9b3091f1154e17d
Naruyoko <32781397+Naruyoko@users.noreply.github.com>
2018-02-27 20:29:32 -0500

    Update expMath.js

--- expMath/expMath.js
+++ expMath/expMath.js
@@ -2,12 +2,12 @@
 var exp=exp||{}
 exp=(function (){
   return{
-    add:function (a,b){return Math.max(a,b)+Math.log10(1+10^(-Math.abs(a-b)));},
-    sub:function (a,b){return Math.max(a,b)+Math.log10(1-10^(-Math.abs(a-b)));},
+    add:function (a,b){return Math.max(a,b)+Math.log10(1+10**(-Math.abs(a-b)));},
+    sub:function (a,b){return Math.max(a,b)+Math.log10(1-10**(-Math.abs(a-b)));},
     mult:function (a,b){return a+b;},
     div:function (a,b){return a-b;},
     pow:function (a,b){return a*10^b;},
     root:function (a,b){return a/10^b;},
-    text:function (a,m){return string(10^(a-Math.floor(a)))+m+string(Math.floor(a));}
+    text:function (a,m){return string(10**(a-Math.floor(a)))+m+string(Math.floor(a));}
   };
 }());
