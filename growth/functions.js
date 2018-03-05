30165320d7d81e8a95e5aeef9056be151ae73fe0
Naruyoko <32781397+Naruyoko@users.noreply.github.com>
2018-03-04 20:50:27 -0500

    Create functions.js

---
+++ growth/functions.js
@@ -0,0 +1,17 @@
+function run(){
+  if (Number(document.input.delay.value)<50){
+    alert("For safety, you need to make\ndelay at least 50 ms!\nYou have: "+document.input.delay.value+" ms");
+    return;
+  }
+  interval=setInterval(increment,Number(document.input.delay.value));
+}
+function increment(){
+  step++;
+  value=rule();
+  document.getElementById("step").textContent=step;
+  document.getElementById("value").textContent=value;
+  if (!isFinite(value)){
+    clearInterval(interval);
+    value.style.color="#FF0000";
+  }
+}
