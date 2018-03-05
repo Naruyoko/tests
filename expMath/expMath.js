13c8157b44893603f1141604c3629a443e597ccc
Naruyoko <32781397+Naruyoko@users.noreply.github.com>
2018-03-05 02:24:12 -0500

    Update expMath.js

--- expMath/expMath.js
+++ expMath/expMath.js
@@ -8,6 +8,32 @@ exp=(function (){
     div:function (a,b){return a-b;},
     pow:function (a,b){return a*10**b;},
     root:function (a,b){return a/10**b;},
+    log:function (a,b){return 
+    tetr:function (a,b){
+      if (b>2){return "Tetrated too large";}
+      var z=10**b-Math.ceil(10**b);
+      var f=Math.log10(1+(2*a*Math.log(10))/(1+a*Math.log(10))*z-(1-a*Math.log(10))/(1+a*Math.log(10))*z**2);
+      if (b>0){
+        for (i=0;i<Math.ceil(10**b);i++){
+          f=exp.pow(a,f);
+        }
+      }
+      if (b<=1){
+        for (i=0;i>Math.ceil(10**b);i--){
+          f=exp.log(f,a);
+        }
+      }
+      return f;
+    },
+    slog:function (a,b){
+      if (b>2){return "Tetrated too large";}
+      var z=10**b-Math.ceil(10**b);
+      var f=Math.log10(1+(2*a*Math.log(10))/(1+a*Math.log(10))*z-(1-a*Math.log(10))/(1+a*Math.log(10))*z**2);
+      for (i=0;i<Math.ceil(10**b);i++){
+        f=exp.pow(a,f);
+      }
+    },
     text:function (a,m){return String(10**Math.abs(a-Math.floor(a)))+m+String(Math.floor(a));}
   };
 }());
+)  2
