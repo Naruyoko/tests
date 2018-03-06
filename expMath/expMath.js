d253c5ad974fbddf8e2544acf4c43c7bd7d8e0d7
Naruyoko <32781397+Naruyoko@users.noreply.github.com>
2018-03-05 19:12:12 -0500

    Update expMath.js

--- expMath/expMath.js
+++ expMath/expMath.js
@@ -10,7 +10,7 @@ exp=(function (){
       }
     },
     sub:function (a,b){return exp.add(a,[b[0],!b[1]]);},
-    mult:function (a,b){return [a[0]+b[0],(!a[1])&&b[1]||a[1]&&(!b[1])];},
+    mult:function (a,b){return [a[0]+b[0],!a[1]&&b[1]||a[1]&&!b[1]];},
     div:function (a,b){return exp.mult(a,[-b[0],b[1]];},
     pow:function (a,b){
       if (a[1]){
@@ -24,7 +24,7 @@ exp=(function (){
       if (a[1]||b[1]){
         return [NaN,false];
       }else{
-        return [Math.log10(Math.abs(a[0]/b[0])),a[0]<0&&(!b[0]<0)||(!a[0]<0)&&b[0]<0];
+        return [Math.log10(Math.abs(a[0]/b[0])),(a[0]<0)&&(b[0]>0)||(a[0]>0)&&(b[0]<0)];
       }
     },
     tetr:function (a,b){
@@ -32,28 +32,29 @@ exp=(function (){
       if (b[0]>2){return [Infinity,false];}
       var B=10**b[0]*-1**b[1]
       var z=B-Math.ceil(B);
-      var f=Math.log10(1+(2*a[0]*Math.log(10))/(1+a[0]*Math.log(10))*z-(1-a[0]*Math.log(10))/(1+a[0]*Math.log(10))*z**2);
+      var f=[Math.log10(1+(2*a[0]*Math.log(10))/(1+a[0]*Math.log(10))*z-(1-a[0]*Math.log(10))/(1+a[0]*Math.log(10))*z**2),false];
       if (B>0){
         for (i=0;i<Math.ceil(B);i++){
-          f=exp.pow(a[0],f);
+          f=exp.pow(a,f);
         }
       }
       if (B<=-1){
         for (i=0;i>Math.ceil(B);i--){
-          f=exp.log(f,a[0]);
+          f=exp.log(f,a);
         }
       }
-      return [f,false];
+      return f;
     },
     slog:function (a,b){
+      if (b[1]){return [NaN,false];}
       var z=a;
-      var f=0;
-      while (z>1){
-        f=exp.add(f,1);
+      var f=[0,false];
+      while ((z[0]<=0)&&!z[1]){
+        f=exp.add(f,[0,false]);
         z=exp.log(z,b);
       }
-      while (z<=0){
-        f=exp.;
+      while ((isFinite(z[0])){
+        f=exp.sub[f,[0,false];
         z=exp.pow(b,z);
       }
       f+=Math.log10(-1+(2*a)/(1+a)*z-(1-a)/(1+a)*z**2);
