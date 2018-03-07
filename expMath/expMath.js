3e954eb7d6b28dc2f30bc72dd1134e69e0588841
Naruyoko <32781397+Naruyoko@users.noreply.github.com>
2018-03-07 12:32:31 -0500

    Update expMath.js

--- expMath/expMath.js
+++ expMath/expMath.js
@@ -4,9 +4,9 @@ exp=(function (){
   return{
     add:function (a,b){
       if (a[1]&&b[1]||(!a[1])&&(!b[1])){
-        return [Math.max(a[0],b[0])+Math.log10(1+10**(-Math.abs(a[0]-b[0]))),a[1]];
+        return [Math.max(a[0],b[0])+Math.log10(1+Math.pow(10,-Math.abs(a[0]-b[0]))),a[1]];
       }else{
-        return [Math.max(a[0],b[0])+Math.log10(1-10**(-Math.abs(a[0]-b[0]))),!a[1]&&(a[0]<b[0])||a[1]&&(a[0]>b[0])];
+        return [Math.max(a[0],b[0])+Math.log10(1-Math.pow(10,-Math.abs(a[0]-b[0]))),!a[1]&&(a[0]<b[0])||a[1]&&(a[0]>b[0])];
       }
     },
     sub:function (a,b){return exp.add(a,[b[0],!b[1]]);},
@@ -16,14 +16,14 @@ exp=(function (){
       if (a[1]){
         return [NaN,false];
       }else{
-        return [a[0]*10**b[0]*-1**b[1],false];
+        return [a[0]*Math.pow(10,b[0])*Math.pow(-1,b[1]),false];
       }
     },
     sqrt:function (a){return exp.root(a,exp.conv(2));},
     cbrt:function (a){return exp.root(a,exp.conv(3));},
     root:function (a,b){return exp.pow(a,[-b[0],b[1]]);},
     log:function (a,b){
-      if (a[1]||b[1]||b[0]=0){
+      if (a[1]||b[1]||(b[0]=0)){
         return [NaN,false];
       }else{
         return [Math.log10(Math.abs(a[0]/b[0])),(a[0]<0)&&(b[0]>0)||(a[0]>0)&&(b[0]<0)];
@@ -31,20 +31,21 @@ exp=(function (){
     },
     fact:function (a){
       if (a[1]){return [NaN,false];}
-      var A=10**a[0];
+      var A=Math.pow(10,a[0]);
       if (A<=30){
         var f=parseFloat("0."+(String(A)).split(".")[1]);
         var x=1;
         var y=1;
         var z=1;
-        for (i=f;i<=A;i++;){
+        var i;
+        for (i=f;i<=A;i++){
           x*=i;
         }
-        for (i=z+1;i<=f-1;i++;){
+        for (i=z+1;i<=f-1;i++){
           y*=i;
         }
-        for (i=1;i<=500;i++){ //middle statement can be modified for precision
-          z*=(1+1/i)**(f)/(1+f/i);
+        for (i=1;i<=500;i++){ //middle statement may be modified for precision
+          z*=Math.pow((1+1/i),(f))/(1+f/i);
         }
         return exp.conv(x/(f*y)*z);
       }else{
@@ -53,15 +54,15 @@ exp=(function (){
     },
     sfac:function(a){
       if (a[1]){return [NaN,false];}
-      if (!ifFinite(10**a[0])){return [Infinity,false];}
+      if (!ifFinite(Math.pow(10,a[0]))){return [Infinity,false];}
       var f=exp.conv(1);
-      for (i=1;i<=Math.round(10**a[0]);i++){
+      for (i=1;i<=Math.round(Math.pow(10,a[0]));i++){
         f=exp.mult(f,exp.fact(exp.conv(i)));
       }
       return f;
     },
     asfc:function (a){
-      if (a[1]1){return [NaN,false];}
+      if (a[1]){return [NaN,false];}
       return exp.tetr(exp.fact(a),exp.fact(a));
     },
     hfac:function (a){
@@ -72,9 +73,10 @@ exp=(function (){
     tetr:function (a,b){
       if (a[1]){return [NaN,false];}
       if (b[0]>2){return [Infinity,false];}
-      var B=10**b[0]*-1**b[1]
+      var B=Math.pow(10,b[0])*Math.pow(-1,b[1]);
       var z=B-Math.ceil(B);
-      var f=[Math.log10(1+(2*a[0]*Math.log(10))/(1+a[0]*Math.log(10))*z-(1-a[0]*Math.log(10))/(1+a[0]*Math.log(10))*z**2),false];
+      var f=[Math.log10(1+(2*a[0]*Math.log(10))/(1+a[0]*Math.log(10))*z-(1-a[0]*Math.log(10))/(1+a[0]*Math.log(10))*Math.pow(z,2)),false];
+      var i;
       if (B>0){
         for (i=0;i<Math.ceil(B);i++){
           f=exp.pow(a,f);
@@ -96,14 +98,13 @@ exp=(function (){
         z=exp.log(z,b);
       }
       while (z[1]||!isFinite(z[0])&&z[0]<0){
-        f=exp.sub[f,[0,false];
+        f=exp.sub(f,[0,false]);
         z=exp.pow(b,z);
       }
-      f=exp.add(f,exp.conv(-1-(2*a)/(1+a)*z+(1-a)/(1+a)*z**2)));
+      f=exp.add(f,exp.conv(-1-(2*a)/(1+a)*z+(1-a)/(1+a)*Math.pow(z,2)));
       return f;
     },
     conv:function (a){return [Math.log10(Math.abs(a)),a<0];},
-    text:function (a,m){return String(10**Math.abs(a-Math.floor(a)))+m+String(Math.floor(a));}
+    text:function (a,m){return String(Math.pow(10,Math.abs(a-Math.floor(a))))+m+String(Math.floor(a));}
   };
 }());
-)  2
