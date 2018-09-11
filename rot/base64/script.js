8efdebbcc91334508b1f4373f9c9af1426325610
Naruyoko <naru519372@gmail.com>
2018-09-11 11:51:13 -0400

    Update script.js

--- rot/base64/script.js
+++ rot/base64/script.js
@@ -1,4 +1,5 @@
 function rotbase64(instr,rot){
+  rot=rot<0?rot%64+64:rot%64;
   const chars="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
   var outstr="";
   for (var i=0;i<instr.length;i++){
