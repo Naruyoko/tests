df57bb1b78525e5bd32b3fd68826ba538d7bf76c
Naruyoko <naru519372@gmail.com>
2022-07-29 20:59:00 -0500

    Bad score function

--- optimizationProblem/minecraft-zigzag/src/minecraft_zigzag_1_8_9/SmallZigzagJudge2.java
+++ optimizationProblem/minecraft-zigzag/src/minecraft_zigzag_1_8_9/SmallZigzagJudge2.java
@@ -4,6 +4,6 @@ public class SmallZigzagJudge2 extends SmallZigzagJudge {
   public double score(Player player) {
     final double xBiasMult=MathHelper.clamp_double(Math.abs((player.posZ+3.5)%4.0-2.0)-1.0,-0.2,0.2);
     final double xBiasBase=xBiasMult<0?-Math.max(player.posX+1.5,-0.2):-Math.min(player.posX+1.5,0.2);
-    return player.posZ-startingState.posZ+xBiasBase*xBiasMult+0.25;
+    return player.posZ-startingState.posZ+xBiasBase*xBiasMult*0.25;
   }
 }
